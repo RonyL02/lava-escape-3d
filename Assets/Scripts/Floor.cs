466c1c002f0b8b4e68565bd7047a163224216212
RonyL02 <280202rony@gmail.com>
2025-05-14 20:15:42 +0300

    change wall spawn

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class Floor : MonoBehaviour
 {
+    float initialSpeed = 0.1f;
+    float acceleration = 0.5f;
+
+    private float currentSpeed;
     // Start is called before the first frame update
     void Start()
     {
-
+        currentSpeed = initialSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.position += Vector3.up * currentSpeed * Time.deltaTime;
+        currentSpeed += acceleration * Time.deltaTime;
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Platform")
+        {
+            Destroy(other.gameObject); // Remove the object that triggered this one
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Wall")
         {
             Destroy(other.gameObject); // Remove the object that triggered this one
         }
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -2,17 +2,19 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    //walls
+    public Material wallMaterial;
     public float wallWidth = 20f;
-    public float wallHeight = 100f;
+    public float wallHeight = 20f;
     public float wallThickness = 0.5f;
+    public int wallsLevel = 5;
 
-
+    //platforms
     public Material platformMaterial;
     public float platformDepth = 1f;
     public float platformWidth = 2f;
     public float platformThickness = 0.3f;
 
-    public float heightDifference = 10f;
 
     public Material floorMaterial;
 
@@ -24,91 +26,101 @@ public class LevelGenerator : MonoBehaviour
     private float lastPlatformHeight;
 
 
+
+    public float spawnInterval = 2f;
+    private float timer = 0f;
+
+    private bool startSpawning = false;
     void Start()
     {
-        currentSpeed = initialSpeed;
-        lastPlatformHeight = heightDifference;
-        GenerateWalls();
-        GeneratePlatforms();
-        AddFloor(this.gameObject, new Vector3(wallWidth, 0.1f, wallWidth));
+        BoxCollider bc = gameObject.AddComponent<BoxCollider>();
+        bc.size = new Vector3(wallWidth, 1, wallWidth);
+        bc.isTrigger = true;
+        for (int i = 0; i < wallsLevel; i++)
+        {
+            GenerateWalls(i * wallHeight);
+        }
     }
 
     void Update()
     {
-        transform.position += Vector3.up * currentSpeed * Time.deltaTime;
-        currentSpeed += acceleration * Time.deltaTime;
-        Debug.Log(lastPlatformHeight);
-        if (Mathf.FloorToInt(wallHeight) + transform.position.y - lastPlatformHeight >= heightDifference)
+        if (startSpawning)
         {
-            GeneratePlatforms();
+            timer += Time.deltaTime;
+
+            if (timer >= spawnInterval)
+            {
+                timer = 0f;
+                GenerateWalls(wallsLevel * wallHeight);
+                wallsLevel++;
+            }
         }
     }
 
-    void GenerateWalls()
+    private void GenerateWalls(float yOffset)
     {
-        CreateWall(new Vector3(0, wallHeight / 2f, wallWidth / 2f), new Vector3(wallWidth, wallHeight, wallThickness));
-        CreateWall(new Vector3(0, wallHeight / 2f, -wallWidth / 2f), new Vector3(wallWidth, wallHeight, wallThickness));
-        CreateWall(new Vector3(-wallWidth / 2f, wallHeight / 2f, 0), new Vector3(wallThickness, wallHeight, wallWidth));
-        CreateWall(new Vector3(wallWidth / 2f, wallHeight / 2f, 0), new Vector3(wallThickness, wallHeight, wallWidth));
+        CreateWall(new Vector3(transform.position.x, transform.position.y + yOffset + wallHeight / 2f, wallWidth / 2f + transform.position.z), new Vector3(wallWidth, wallHeight, wallThickness));
+        CreateWall(new Vector3(transform.position.x, transform.position.y + yOffset + wallHeight / 2f, -wallWidth / 2f + transform.position.z), new Vector3(wallWidth, wallHeight, wallThickness));
+        CreateWall(new Vector3(-wallWidth / 2f + transform.position.x, transform.position.y + yOffset + wallHeight / 2f, transform.position.z), new Vector3(wallThickness, wallHeight, wallWidth));
+        CreateWall(new Vector3(wallWidth / 2f + transform.position.x, transform.position.y + yOffset + wallHeight / 2f, transform.position.z), new Vector3(wallThickness, wallHeight, wallWidth));
+        GeneratePlatformOnWall(yOffset);
     }
 
-    void CreateWall(Vector3 position, Vector3 scale)
+    private void CreateWall(Vector3 position, Vector3 scale)
     {
         GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
         wall.transform.position = position;
         wall.transform.localScale = scale;
         wall.transform.parent = this.transform; // optional: keep hierarchy clean
         wall.name = "Wall";
-    }
-
-    void GeneratePlatforms()
-    {
-        for (float platformHeight = lastPlatformHeight; platformHeight < Mathf.FloorToInt(wallHeight) + transform.position.y; platformHeight += heightDifference)
+        Rigidbody rb = wall.AddComponent<Rigidbody>();
+        rb.isKinematic = true;
+        if (wallMaterial != null)
         {
-            lastPlatformHeight = platformHeight;
-            float y = platformHeight;
-
-
-            int wallIndex = Random.Range(0, 4); // 0 = Front, 1 = Back, 2 = Left, 3 = Right
-            float halfWidth = wallWidth / 2f;
-            float halfThickness = wallThickness / 2f;
-
-            switch (wallIndex)
-            {
-                case 0: // Front (Z+), random X
-                    CreatePlatform(
-                        new Vector3(Random.Range(-halfWidth + platformWidth / 2f, halfWidth - platformWidth / 2f), y, halfWidth - halfThickness - platformDepth / 2f),
-                        new Vector3(platformWidth, platformThickness, platformDepth)
-                    );
-                    break;
-
-                case 1: // Back (Z-), random X
-                    CreatePlatform(
-                        new Vector3(Random.Range(-halfWidth + platformWidth / 2f, halfWidth - platformWidth / 2f), y, -halfWidth + halfThickness + platformDepth / 2f),
-                        new Vector3(platformWidth, platformThickness, platformDepth)
-                    );
-                    break;
-
-                case 2: // Left (X-), random Z
-                    CreatePlatform(
-                        new Vector3(-halfWidth + halfThickness + platformDepth / 2f, y, Random.Range(-halfWidth + platformWidth / 2f, halfWidth - platformWidth / 2f)),
-                        new Vector3(platformDepth, platformThickness, platformWidth)
-                    );
-                    break;
-
-                case 3: // Right (X+), random Z
-                    CreatePlatform(
-                        new Vector3(halfWidth - halfThickness - platformDepth / 2f, y, Random.Range(-halfWidth + platformWidth / 2f, halfWidth - platformWidth / 2f)),
-                        new Vector3(platformDepth, platformThickness, platformWidth)
-                    );
-                    break;
-            }
-
+            Renderer renderer = wall.GetComponent<Renderer>();
+            renderer.material = wallMaterial;
         }
-        lastPlatformHeight += heightDifference;
     }
 
-    void CreatePlatform(Vector3 position, Vector3 scale)
+    private void GeneratePlatformOnWall(float y)
+    {
+        int wallIndex = Random.Range(0, 4); // 0 = Front, 1 = Back, 2 = Left, 3 = Right
+        float halfWidth = wallWidth / 2f;
+        float halfThickness = wallThickness / 2f;
+
+        switch (wallIndex)
+        {
+            case 0: // Front (Z+), random X
+                CreatePlatform(
+                    new Vector3(Random.Range(transform.position.x - halfWidth + platformWidth / 2f, transform.position.x + halfWidth - platformWidth / 2f), y, transform.position.z + halfWidth - halfThickness - platformDepth / 2f),
+                    new Vector3(platformWidth, platformThickness, platformDepth)
+                );
+                break;
+
+            case 1: // Back (Z-), random X
+                CreatePlatform(
+                    new Vector3(Random.Range(transform.position.x - halfWidth + platformWidth / 2f, transform.position.x + halfWidth - platformWidth / 2f), y, transform.position.z - halfWidth + halfThickness + platformDepth / 2f),
+                    new Vector3(platformWidth, platformThickness, platformDepth)
+                );
+                break;
+
+            case 2: // Left (X-), random Z
+                CreatePlatform(
+                    new Vector3(transform.position.x - halfWidth + halfThickness + platformDepth / 2f, y, Random.Range(transform.position.z - halfWidth + platformWidth / 2f, transform.position.z + halfWidth - platformWidth / 2f)),
+                    new Vector3(platformDepth, platformThickness, platformWidth)
+                );
+                break;
+
+            case 3: // Right (X+), random Z
+                CreatePlatform(
+                    new Vector3(transform.position.x + halfWidth - halfThickness - platformDepth / 2f, y, Random.Range(transform.position.z - halfWidth + platformWidth / 2f, transform.position.z + halfWidth - platformWidth / 2f)),
+                    new Vector3(platformDepth, platformThickness, platformWidth)
+                );
+                break;
+        }
+    }
+
+    private void CreatePlatform(Vector3 position, Vector3 scale)
     {
         GameObject platform = GameObject.CreatePrimitive(PrimitiveType.Cube);
         platform.transform.position = position;
@@ -125,13 +137,14 @@ public class LevelGenerator : MonoBehaviour
         }
     }
 
-    void AddFloor(GameObject parent, Vector3 size)
+    private void AddFloor(GameObject parent, Vector3 size)
     {
         GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Cube);
         floor.name = "Floor";
+        floor.tag = "lava";
         floor.transform.parent = parent.transform;
         floor.transform.localScale = size;
-        floor.transform.localPosition = new Vector3(0, -size.y / 2f, 0);
+        floor.transform.localPosition = new Vector3(0, -wallHeight, 0);
 
         floor.AddComponent<Floor>();
 
@@ -144,5 +157,18 @@ public class LevelGenerator : MonoBehaviour
             Renderer renderer = floor.GetComponent<Renderer>();
             renderer.material = floorMaterial;
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            startSpawning = true;
+            AddFloor(this.gameObject, new Vector3(wallWidth, 0.1f, wallWidth));
+            BoxCollider bc = GetComponent<BoxCollider>();
+            bc.enabled = false;
+            Debug.Log("aaaaaaaaaaaaa");
+        }
+
     }
 }

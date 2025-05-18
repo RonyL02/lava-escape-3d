a9cba58b1b8043c3b446d5df9e9c3b169e174d1e
Asaf <asaflumer@gmail.com>
2025-05-18 21:55:25 +0300

    fixed jump height

--- Assets/MakePlayerJumpHigher.cs
+++ Assets/MakePlayerJumpHigher.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class MakePlayerJumpHigher : MonoBehaviour
 {
     public FmsScript player;
+    public float jumpSpeed = 28;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            player.jumpSpeed = 26;
+            player.jumpSpeed = jumpSpeed;
         }
     }
 }
